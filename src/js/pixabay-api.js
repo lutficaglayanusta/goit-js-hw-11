d8c301d911df79e7669187591d21d585c05f32e0
caglayan <caglayanustaa@gmail.com>
2025-07-03 15:31:46 +0300

    Refactor fetchImages function to use axios for API calls; improve error handling

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,19 +1,22 @@
-
+import axios from 'axios';
 const api_key = '51088577-7b521529318281431558696f8';
 
 export const fetchImages = (input) => { 
 
     const api_url = `https://pixabay.com/api/?key=${api_key}&q=${input}&image_type=photo&orientation=horizontal&safesearch=true`;
 
-    return fetch(api_url)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .catch(error => {
-            console.error('There has been a problem with your fetch operation:', error);
-        });
-
+    axios.get(api_url).then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        
+        if (response.data.hits.length > 0) {
+            return response.data.hits;
+        } else {
+            throw new Error('No images found');
+        }
+    }).catch(error => { 
+        console.error('Error fetching images:', error);
+        throw error;
+    });
 }
